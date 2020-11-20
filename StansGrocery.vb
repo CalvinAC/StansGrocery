0f3ea90f20d4ddb0bd0fc39bf291911ed2f5b98a
cameron coxen <coxecame@isu.edu>
2020-11-19 18:56:43 -0700

    Finished program

--- StansGrocery.vb
+++ StansGrocery.vb
@@ -18,16 +18,24 @@ Public Class StansGroceryForm
         Dim secondArray() As String
         Dim thirdArray() As String
 
+        'Splits the array 
         temp = Split(My.Resources.Grocery, vbLf)
 
         For i = LBound(temp) To UBound(temp) - 1
+            'Creates second array from the first split array
+            'Sorts by commas
             secondArray = Split(temp(i), Chr(34) & "," & Chr(34))
+            'Creates a third array, filters items from textfile by "$$ITM"
+            'Stores filtered items into the thirdarray
             thirdArray = Split(secondArray(0), "$$ITM")
             Console.Write(thirdArray(1))
+            'The thirdarray is sent to the global food array
             Me.food$(i, 0) = thirdArray(1)
+            'Filters items from textfile by "##LOC"
             thirdArray = Split(secondArray(1), "##LOC")
             Console.Write(thirdArray(1))
             Me.food$(i, 1) = thirdArray(1).PadLeft(2)
+            'Filters items from textfile by "%%CAT"
             thirdArray = Split(secondArray(2), "%%CAT")
             thirdArray = Split(thirdArray(1), Chr(34))
             Console.WriteLine(thirdArray(0))
@@ -36,35 +44,11 @@ Public Class StansGroceryForm
 
     End Sub
 
-    Sub loadComboBox()
-        If CategoryButton.Checked = True Then
-            filter = 2
-        ElseIf AisleButton.Checked = True Then
-            filter = 1
-        End If
-
-        Try
-
-            DisplayComboBox.Items.Clear()
-            For i = LBound(food) To UBound(food) - 1
-                If food(i, filter) <> "" And food$(i, filter) <> "  " And Not DisplayComboBox.Items.Contains(food(i, filter)) Then
-                    DisplayComboBox.Items.Add(food(i, filter))
-
-                End If
-
-            Next
-
-        Catch ex As Exception
-
-        End Try
-
-
-    End Sub
-
     Sub LoadListBox()
 
         For i = LBound(Me.food$) To UBound(Me.food$) - 1
 
+            'Loads the list box with the data stored in food array
             If food(i, 0) <> "" And DisplayListBox.Items.Contains(food(i, 0)) = False Then
                 DisplayListBox.Items.Add(food(i, 0))
             End If
@@ -79,17 +63,14 @@ Public Class StansGroceryForm
         CategoryButton.Checked = True
     End Sub
 
-    Private Sub SelectFormToolStripMenuItem_Click(sender As Object, e As EventArgs)
-        SplashScreenForm.Show()
-    End Sub
-
     Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click, SearchToolStripMenuItem.Click
-        loadComboBox()
+
         DisplayListBox.Items.Clear()
         Dim searchString As String = SearchBox.Text
 
         Try
             For i = LBound(Me.food$) To UBound(Me.food$) - 1
+                'Compares text of the search box with any matching text in the food array and displays it into the listbox
                 If InStr(Me.food$(i, 0), searchString, CompareMethod.Text) <> 0 Or InStr(Me.food$(i, 2), searchString, CompareMethod.Text) > 0 Then
                     DisplayListBox.Items.Add(Me.food$(i, 0))
                 End If
@@ -101,8 +82,9 @@ Public Class StansGroceryForm
     End Sub
 
     Private Sub CategoryButton_CheckedChanged(sender As Object, e As EventArgs) Handles CategoryButton.CheckedChanged
+
+        'Loads data from the 3rd column of the food array into the combo box
         If CategoryButton.Checked = True Then
-            'loadComboBox()
             DisplayComboBox.Items.Clear()
             For i = LBound(food) To UBound(food) - 1
                 If food(i, 2) <> "" And DisplayComboBox.Items.Contains(food(i, 2)) = False Then
@@ -120,10 +102,10 @@ Public Class StansGroceryForm
     End Sub
 
     Private Sub AisleButton_CheckedChanged(sender As Object, e As EventArgs) Handles AisleButton.CheckedChanged
+
+        'Loads data from the 2nd cloumn of the food array into the combo box
         If AisleButton.Checked = True Then
-
             DisplayComboBox.Items.Clear()
-
             For i = LBound(Me.food$) To UBound(Me.food$) - 1
                 If food(i, 1) <> "  " And DisplayComboBox.Items.Contains(food(i, 1)) = False Then
                     DisplayComboBox.Items.Add(food(i, 1))
@@ -139,13 +121,13 @@ Public Class StansGroceryForm
 
     Private Sub SearchBox_TextChanged(sender As Object, e As EventArgs) Handles SearchBox.TextChanged
 
-        If SearchBox.TextLength < 2 Then
+
+        DisplayLabel.Text = Nothing
+        If SearchBox.TextLength = 1 Then
             DisplayLabel.Text = "Please be more specific."
-            Exit Sub
         ElseIf SearchBox.Text = "zzz" Then
             Me.Close()
         Else
-
         End If
 
     End Sub
@@ -155,11 +137,36 @@ Public Class StansGroceryForm
         Me.Hide()
     End Sub
 
+    Private Sub DisplayComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayComboBox.SelectedIndexChanged
+
+        'Loads the combo box based on which radio button is selected
+        DisplayListBox.Items.Clear()
+        If CategoryButton.Checked = True Then
+            For i = LBound(Me.food$) To UBound(Me.food$) - 1
+                If DisplayComboBox.SelectedItem.ToString = food(i, 2) Then
+                    DisplayListBox.Items.Add(food(i, 0))
+                End If
+            Next
+
+        ElseIf AisleButton.Checked = True Then
+            For i = LBound(Me.food$) To UBound(Me.food$) - 1
+                If DisplayComboBox.SelectedItem.ToString = food(i, 1) Then
+                    DisplayListBox.Items.Add(food(i, 0))
+                End If
+            Next
+
+        End If
+
+    End Sub
+
     Private Sub DisplayListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayListBox.SelectedIndexChanged
 
+        'Iterates through the food array 
         For a = 0 To 255
             For b = 0 To 2
+                'Searches for item selcted in the display box
                 If DisplayListBox.SelectedItem.ToString = food(a, b) Then
+                    'Displays text telling where to find the item
                     DisplayLabel.Text = "You will find " & food(a, b) & " on Aisle " &
                         food(a, b + 1) & " With the " & food(a, b + 2)
                 End If
@@ -168,17 +175,7 @@ Public Class StansGroceryForm
         Next
     End Sub
 
-    Private Sub DisplayComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayComboBox.SelectedIndexChanged
-        'DisplayLabel.Text = "Stuff"
-        For a = 0 To 255
-            For b = 0 To 2
-                If DisplayComboBox.SelectedIndex.ToString = food(a, b) Then
-                    DisplayListBox.Items.Add(food(a, b))
-                End If
-
-            Next
-        Next
-
-        'DisplayListBox.Items.Clear()
+    Private Sub SelectFormToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        SplashScreenForm.Show()
     End Sub
 End Class
